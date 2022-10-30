5e2d3ee952f26f6f63923a4461056c6ab3049d52
YungKENNY <64759152+YungKENNY@users.noreply.github.com>
2022-10-30 09:29:31 +0300

    Add files via upload

---
+++ POST_LR/src/test/java/Pojo/UserPojoFull.java
@@ -0,0 +1,23 @@
+package Pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserPojoFull {
+    private int id;
+    private String avatar;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private String email;
+
+    public  String getLastName(){return lastName;}
+
+    public int getId(){return id;}
+
+    public  String getAvatar(){return avatar;}
+    public String getFirstName(){return firstName;}
+    public  String getEmail(){return email;}
+
+}
+
