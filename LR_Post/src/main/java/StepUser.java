ce2621d048f9da098939cedcf339ab7486737a0b
YungKENNY <64759152+YungKENNY@users.noreply.github.com>
2022-10-30 09:30:25 +0300

    Add files via upload

---
+++ LR_Post/src/main/java/StepUser.java
@@ -0,0 +1,11 @@
+import Model.LoginRequestModel;
+public class StepUser {
+
+    ApiUser apiUser = new ApiUser();
+
+    public void createLogin(String login, String pass) {
+        LoginRequestModel model = new LoginRequestModel(login,pass);
+        apiUser.createLogin(model);
+    }
+
+}
---
+++ LR_Post/src/test/java/TestCreateLogin.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestCreateLogin {
+    StepUser stepUser=new StepUser();
+    @Test
+    public void LoginTest(){
+        stepUser.createLogin("log", "pass");
+    }
+}
